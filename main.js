3b5a63aef4b0361d34517d8842c55db89aa63dc6
Juan Arbelaez <juanarbelaez@Juans-MacBook-Pro.local>
2015-11-14 08:24:35 -0500

    firstcommit/

---
+++ main.js
@@ -0,0 +1,14 @@
+function ready(fn) {
+
+  if(typeof fn !== 'function') return;
+
+  if(document.readyState === "complete" || document.readyState === "interactive") {
+    return fn();
+  }
+  document.addEventListener('DOMContentLoaded', fn, false);
+}
+
+ready(function () {
+  var childArr = [].slice.call(document.body.children);
+  readTime.configWPM(500);
+  document.querySelector('h3.reading-time').innerHTML = readTime.words(childArr);
